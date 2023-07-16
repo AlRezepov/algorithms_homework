1267ccddf6e194237a16eb40ead6cceb044b12f5
AlRezepov <alexrezepovjr@mail.ru>
2023-07-16 12:18:23 +0700

    Add homework_02

---
+++ 02/task_02/task_02.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+
+int fibonacci(int n) {
+    std::vector<int> fib(n + 1);
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[n];
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int n;
+    std::cout << "Введите число: ";
+    std::cin >> n;
+    std::cout << "Число Фибоначчи = " << fibonacci(n);
+
+    return 0;
+}
